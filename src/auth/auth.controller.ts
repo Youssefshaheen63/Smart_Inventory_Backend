b574cb0bb628536b4a807d60170fb8848759c0b8
MOSTAFA-MANSOUR72 <mostafamansour76272@gmail.com>
2026-07-15 09:04:22 +0300

    feat(auth): implement JWT authentication with login and register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,1 +1,23 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { LoginUserDto } from './dto/login-dto';
+import { Public } from './public.decorator';
 
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Public()
+  @Post('register')
+  async register(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.signup(createUserDto);
+  }
+
+  @Public()
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    const { emailOrUsername, password } = loginUserDto;
+    return await this.authService.signIn(emailOrUsername, password);
+  }
+}
