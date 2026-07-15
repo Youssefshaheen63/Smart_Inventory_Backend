5d46851d5763fdfc10dd01e0e1d28db22933bfb5
Ali-Ragab214 <alighonim78@gmail.com>
2026-07-16 01:57:53 +0300

    feat(inventory): add StockMovement entity and MovementReason enum

---
+++ src/inventory/stock-movements/enums/movement-reason.enum.ts
@@ -0,0 +1,12 @@
+
+export enum MovementReason {
+  PURCHASE_ORDER_RECEIPT = 'purchase_order_receipt',
+  SALE = 'sale',
+  MANUAL_ADJUSTMENT = 'manual_adjustment',
+  WRITE_OFF = 'write_off',
+  CUSTOMER_RETURN = 'customer_return',
+  SUPPLIER_RETURN = 'supplier_return',
+  TRANSFER_IN = 'transfer_in',
+  TRANSFER_OUT = 'transfer_out',
+  AGENT_REORDER = 'agent_reorder',
+}
