0e3e42f4b6f470c143d07ca7d6862cff91dfa929
Ali-Ragab214 <alighonim78@gmail.com>
2026-07-16 12:54:29 +0300

    Update vendor entity column types and refresh lockfile

--- src/vendors/entities/vendor.entity.ts
+++ src/vendors/entities/vendor.entity.ts
@@ -6,9 +6,9 @@ export class Vendor extends AbstractEntity {
   @Column({ length: 255 })
   name!: string;
 
-  @Column({ length: 255, nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   contactEmail!: string | null;
 
-  @Column({ length: 50, nullable: true })
+  @Column({ type: 'varchar', length: 50, nullable: true })
   contactPhone!: string | null;
 }
