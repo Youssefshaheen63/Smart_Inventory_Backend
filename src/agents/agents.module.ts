7574ecfb277a9dbfc09c00dd6996807dffe372ae
OmarAdel898 <omar.97453@cs.mti.edu.eg>
2026-07-21 22:57:35 +0300

    feat: register and export LLMService in AgentsModule

--- src/agents/agents.module.ts
+++ src/agents/agents.module.ts
@@ -1,4 +1,3 @@
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgentRun } from './entities/agent-run.entity';
@@ -6,6 +5,7 @@ import { AgentStep } from './entities/agent-step.entity';
 import { ApprovalRequest } from './entities/approval-request.entity';
 import { ApprovalQueueService } from './approval-queue.service';
 import { ApprovalQueueController } from './approval-queue.controller';
+import { LLMService } from './llm.service';
 import { AgentRunService } from './agent-run.service';
 import { AgentRunMapper } from './mappers/agent-run.mapper';
 import { ApprovalRequestMapper } from './mappers/approval-request.mapper';
@@ -13,7 +13,7 @@ import { ApprovalRequestMapper } from './mappers/approval-request.mapper';
 @Module({
   imports: [TypeOrmModule.forFeature([AgentRun, AgentStep, ApprovalRequest])],
   controllers: [ApprovalQueueController],
-  providers: [ApprovalQueueService, AgentRunService, AgentRunMapper, ApprovalRequestMapper],
-  exports: [ApprovalQueueService, AgentRunService],
+  providers: [LLMService,ApprovalQueueService, AgentRunService, AgentRunMapper, ApprovalRequestMapper],
+  exports: [LLMService,ApprovalQueueService, AgentRunService],
 })
 export class AgentsModule {}
